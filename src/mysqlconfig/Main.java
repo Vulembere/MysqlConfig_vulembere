05b8c084017b2d7c4b20fd965419126fadbef781
vulembere <willsonantoine@outlook.fr>
2020-06-04 21:57:50 +0200

    Classe principal de test

---
+++ src/mysqlconfig/Main.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mysqlconfig;
+
+/**
+ *
+ * @author wills
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MysqlConfig_vulembere config = new MysqlConfig_vulembere("localhost", "mysql", "root", "", 3306);
+        config.newUser("Nouvelle BD", "Nouveau Utilisateur", "Son mot de passe");
+        if (config.getCon() != null) {
+            config.createDatabase();
+            config.createUser();
+            config.addAllPrivilege();
+            config.Autoriser();
+            System.out.println(" traitement succed ");
+        } else {
+            System.out.println("Erreur de connexion");
+        }
+    }
+
+}
